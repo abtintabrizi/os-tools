44f7eb489fe7aa6102b5982a744d1ea645066298
abtintabrizi <abtin.tabrizi2@gmail.com>
2026-03-31 01:37:45 -0400

    Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: 'os-tools',
+  base: '/os-tools',
   plugins: [react()],
   resolve: {
     alias: {
