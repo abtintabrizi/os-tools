62638055e4292918c8c158ccec78ae8cf2167a1d
abtintabrizi <tabrizi2002@yahoo.com>
2026-04-02 16:55:25 -0400

    Updated spec UI

--- frontend/src/features/common/constants.ts
+++ frontend/src/features/common/constants.ts
@@ -2,42 +2,52 @@ export const ALL_MAPS = [
   {
     name: "Ahten City",
     image: "/Ahten_City.png",
+    icon: "/Ahten_City_Icon.png",
   },
   {
     name: "Aimi's App",
     image: "/AiMis_App.png",
+    icon: "/AiMis_App_Icon.png",
   },
   {
     name: "Atlas's Lab",
     image: "/Atlas_Lab.png",
+    icon: "/Atlas_Lab_Icon.png",
   },
   {
     name: "Clarion Test Chamber",
     image: "/Clarion_Test_Chamber.png",
+    icon: "/Clarion_Test_Chamber_Icon.png",
   },
   {
     name: "Demon Dais",
     image: "/Demon_Dais.png",
+    icon: "/Demon_Dais_Icon.png",
   },
   {
     name: "Gates of Obscura",
     image: "/Gates_of_Obscura.png",
+    icon: "/Gates_of_Obscura_Icon.png",
   },
   {
     name: "Inky's Splash Zone",
     image: "/Inkys_Splash_Zone.png",
+    icon: "/Inkys_Splash_Zone_Icon.png",
   },
   {
     name: "Night Market",
     image: "/Night_Market.png",
+    icon: "/Night_Market_Icon.png",
   },
   {
     name: "Oni Village",
     image: "/Oni_Village.png",
+    icon: "/Oni_Village_Icon.png",
   },
   {
     name: "Taiko Temple",
     image: "/Taiko_Temple.png",
+    icon: "/Taiko_Temple_Icon.png",
   },
 ];
 
