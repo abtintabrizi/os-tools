c083341d9684bc8b468b90b662aaf6cf1dcc2203
abtintabrizi <tabrizi2002@yahoo.com>
2026-09-25 18:18:50 -0400

    Fix for preview builds

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,6 +27,7 @@ ALLOWED_ORIGINS = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
+    allow_origin_regex=r"^https://os-tools-[a-z0-9-]+-os-tools\.vercel\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
